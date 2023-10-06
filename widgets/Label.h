3b62b03d90944a873250ead660db347698b6c810
nick weishaar <n.weishaar@richmondelectronicdesign.com>
2023-10-05 20:44:32 -0400

    master - subproperties working

--- widgets/Label.h
+++ widgets/Label.h
@@ -2,12 +2,12 @@
 #include "BaseWidget.h"
 
 class Label : public BaseWidget {
+   GFCSDRAW_OBJECT(Label, BaseWidget)
 public:
    Label(std::string name);
    void render() const override;
    void _process(float dt) override;
    void registerProperties() override;
-   static std::shared_ptr<Label> deserialize(std::string instanceName, PropertyPrototypeMap);
 
 protected:
    StringProperty text;
