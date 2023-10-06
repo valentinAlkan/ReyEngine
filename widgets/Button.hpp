20ae453b8341b24d8e89f0297d69dc924069bc22
nw <none@example.com>
2023-10-06 13:02:50 -0400

    master - better macro def

--- widgets/Button.hpp
+++ widgets/Button.hpp
@@ -2,12 +2,17 @@
 #include "Control.hpp"
 
 class BaseButton : public Control {
-   GFCSDRAW_OBJECT_SIMPLE(BaseButton, Control){}
+   GFCSDRAW_OBJECT(BaseButton, Control){}
 public:
    void registerProperties() override;
 };
 
-class PushButton : public BaseWidget{
+class PushButton : public BaseButton{
+   GFCSDRAW_OBJECT(PushButton, BaseButton)
+   , text("text"){}
+   void registerProperties() override {};
+   void render() const override {}
+   StringProperty text;
 
 };
 
