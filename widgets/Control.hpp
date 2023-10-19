8ed1ea1934f1e6608f68a0c6e72171cf4d8755ae
nw <none@example.com>
2023-10-18 20:41:16 -0400

    master - misc

--- widgets/Control.hpp
+++ widgets/Control.hpp
@@ -3,18 +3,6 @@
 
 class Control : public BaseWidget {
 public:
-//   static std::shared_ptr<BaseWidget> Control::deserialize(const std::string &instanceName,
-//                                                           PropertyPrototypeMap &properties) {
-//      __CTOR_RECT__ = {0,0,0,0};
-//      auto retval = std::make_shared<Control>(instanceName, r);
-//      retval->BaseWidget::_deserialize(properties);
-//      return retval;
-//   }
-//   Control(std::string name, GFCSDraw::Rect<float> r):
-//   Control(std::move(name),"Control",r){}
-//protected:
-//   Control(std::string name, std::string typeName, __CTOR_RECT__):
-//   BaseWidget(name, std::move(typeName), r){}
    GFCSDRAW_OBJECT(Control, BaseWidget){}
 public:
    void render() const override {}
