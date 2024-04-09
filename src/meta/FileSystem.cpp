cc7c330be958dd6bdb8506e3de8caa7cade77e7c
nw <none@example.com>
2024-04-08 21:49:13 -0400

    19-create-a-csv-parser - added byte count reading

--- src/meta/FileSystem.cpp
+++ src/meta/FileSystem.cpp
@@ -1,6 +1,5 @@
 #include "FileSystem.h"
 #include <iostream>
-#include <fstream>
 #include <cstring>
 #include <filesystem>
 #include "Application.h"
@@ -16,15 +15,39 @@ std::vector<char> ReyEngine::FileSystem::readFile(const std::string &filePath) {
 
    auto end = ifs.tellg();
    ifs.seekg(0, std::ios::beg);
-
    auto size = std::size_t(end - ifs.tellg());
-
    if (size == 0) return {};
 
    std::vector<char> buffer(size);
 
    if (!ifs.read((char *) buffer.data(), buffer.size()))
       throw std::runtime_error(filePath + ": " + std::strerror(errno));
+   return buffer;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+void ReyEngine::FileSystem::File::open() {
+   _ifs = std::ifstream(path, std::ios::binary | std::ios::ate);
+   if (!_ifs){
+      throw std::runtime_error(path + ": " + std::strerror(errno));
+   }
+   _end = _ifs.tellg();
+   _ptr = 0;
+   _ifs.seekg(_ptr, std::ios::beg);
+   _open = true;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+std::vector<char> ReyEngine::FileSystem::File::readBytes(long long count) {
+   if (!_open) throw std::runtime_error("File " + abs() + " is not open!");
+   auto size = std::size_t(_end - _ptr);
+   if (size == 0) return {};
+   if (count > size) count = size;
+   std::vector<char> buffer(count);
+   _ifs.seekg(_ptr, std::ios::beg);
+   if (!_ifs.read((char *) buffer.data(), buffer.size()))
+      throw std::runtime_error(path + ": " + std::strerror(errno));
+   _ptr += count;
    return buffer;
 }
 
