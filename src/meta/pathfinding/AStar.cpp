c64b1f51c3f75a504fbaee35ec0af5b90e6dd2a5
mgarvin <garvinmw@gmail.com>
2024-05-29 17:38:52 -0400

    save progress

--- src/meta/pathfinding/AStar.cpp
+++ src/meta/pathfinding/AStar.cpp
@@ -1,5 +1,8 @@
-//
-// Created by garvi on 5/28/2024.
-//
-
 #include "AStar.h"
+
+float AStar::getHeuristic(SearchNode node) {
+   float heuristic = sqrt((_xGoal - node.x_coord)*(_xGoal - node.x_coord) + (_yGoal - node.y_coord) * (_yGoal - node.y_coord));
+   return heuristic;
+}
+
+
--- src/meta/pathfinding/AStar.h
+++ src/meta/pathfinding/AStar.h
@@ -4,8 +4,31 @@
 #include <vector>
 #include <queue>
 
+//struct for comparing search nodes for the priority queue
+struct Comparator{
+   inline bool operator()(const SearchNode a, const SearchNode b){
+      return a.combinedDistance > b.combinedDistance;
+   }
+};
 
 //Class to perform AStar search
 class AStar {
+public:
+   std::priority_queue<SearchNode, vector<SearchNode>,Comparator> expandSet;
 
+   /**
+    * places all connections in the priority queue and overwrites parents if the combined distance is less
+    */
+   void expandNode(SearchNode node);
+
+   /**
+    * gets the heuristic for that node to the goal
+    * @param node : node to calculate heuristic for
+    * @return : the heuristic
+    */
+    float getHeuristic(SearchNode node);
+
+private:
+   int _xStart, _yStart;
+   int _xGoal, _yGoal;
 };
