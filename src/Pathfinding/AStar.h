ba04142b4ead2d49c50cf7f762d9f711d4204847
nw <none@example.com>
2024-05-30 20:15:52 -0400

    save progress

--- src/Pathfinding/AStar.h
+++ src/Pathfinding/AStar.h
@@ -8,7 +8,12 @@ struct coord{
 class AStar {
 public:
    AStar(coord start, coord goal);
-   AStar(coord start, coord goal, SearchNode[][] *map);
 
+private:
+   //Map that is the search, first int is y, second is x
+   std::map<int, std::map<int, SearchNode>> _searchMap;
 
+   std::priority_queue<SearchNode, std::vector<SearchNode>, std::greater<SearchNode>> _expandedNodes;
+   std::map<int, SearchNode> _closedSet;
+   std::map<int, SearchNode> _openSet;
 };
