e06c3e8494180f999bab9fed318b1708244d18f0
nw <none@example.com>
2024-06-01 08:06:58 -0400

    save progress

--- src/Pathfinding/SearchNode.cpp
+++ src/Pathfinding/SearchNode.cpp
@@ -2,8 +2,25 @@
 
 void SearchNode::updateParent(SearchNode *_parent, float _cost) {
    //old cost is greater than new cost - update parent
-   if(cost > _cost){
+   if(cost > _cost + baseCost){
       parent = _parent;
-      cost = _cost;
+      cost = _cost + baseCost;
    }
+}
+
+void SearchNode::setHeuristic(float _heuristic) {
+   heuristic = _heuristic;
+}
+
+void SearchNode::setCost(float _cost, float costConnection) {
+   cost = _cost + baseCost * costConnection;
+}
+
+void SearchNode::calculateCombinedCost() {
+   combinedCost = cost + heuristic;
+}
+
+void SearchNode::addConnection(float cost, SearchNode *connection) {
+   //todo: check to see if the connection exists
+   connections.insert({connection, cost});
 }
