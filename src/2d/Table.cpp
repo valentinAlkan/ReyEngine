b6d3b55b699c0514432e00cceee897d468b5f352
mgarvin <garvinmw@gmail.com>
2024-06-07 15:22:52 -0400

    save progress, my brain hurts

--- src/2d/Table.cpp
+++ src/2d/Table.cpp
@@ -1,5 +1,47 @@
 #include "Table.h"
-
+////////////////////////////////////////////////////////////////////////////////////
 void ReyEngine::Table::setColumnCount(int numOfColumns){
-   column
+   _columnCount = numOfColumns;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+void ReyEngine::Table::addRow(std::vector<std::string> data) {
+   auto lastRow = _data.end()--;
+   int rowId = lastRow->first;
+   std::map<int, std::string> tempMap;
+   for(int i = 0; i < data.size(); i++){
+      tempMap.insert({i, data.at(i)});
+   }
+   _data.insert({rowId, tempMap});
+   _rowCount++;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+void ReyEngine::Table::setColumnNames(std::vector<std::string> names) {
+   std::map<int, std::string> tempMap;
+   for(int i = 0; i < names.size(); i++){
+      tempMap.insert({i, names.at(i)});
+      setColumnCount(i + 1);
+   }
+   _data[0] = tempMap;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+void ReyEngine::Table::setColumnTitle(int col, std::string title) {
+   _data[0][col] = title;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+void ReyEngine::Table::_init_() {
+   _backPane = std::make_shared<Control>(std::to_string(Application::generateUniqueValue()) + "TableBackPane");
+   _backPane->setAnchoring(BaseWidget::Anchor::FILL);
+   addChild(_backPane);
+   _tableGrid = std::make_shared<GridLayout>(std::to_string(Application::generateUniqueValue()) + "TableGridLayout");
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+void ReyEngine::Table::updateTable() {
+   _tableGrid->
+   for(int i = 0; i < _columnCount; i++){
+   }
 }
--- src/2d/Table.h
+++ src/2d/Table.h
@@ -4,8 +4,9 @@
 #include "Layout.h"
 
 namespace ReyEngine {
-   class Table : public TileMap {
+   class Table : public Canvas {
    public:
+      void _init_();
       /**
        * sets the column count
        * @param numOfColumns : Number of columns desired
@@ -31,10 +32,21 @@ namespace ReyEngine {
        */
       void setColumnTitle(int col, std::string title);
    private:
+      int _columnCount;
+      int _rowCount;
+      int _rowHeight;
+      std::shared_ptr<Control> _backPane;
+      std::shared_ptr<GridLayout> _tableGrid;
+
+      std::map<int, int> _columnWidths;
+
       std::map<int, std::string> _columnTitles;
 
       // first index is the row number, second is the column number
       std::map<int, std::map<int, std::string>> _data;
+
+      //updates the table to display the data
+      void updateTable();
 
    };
 }
