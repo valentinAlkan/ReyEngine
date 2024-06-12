dc50485696275f42298e634cbd647a7ec3c3ccb9
nick weishaar <n.weishaar@richmondelectronicdesign.com>
2024-06-11 22:52:16 -0400

    master - camera tests

--- test/raylibTest.cpp
+++ test/raylibTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "DrawInterface.h"
+#include "InputManager.h"
 
 using namespace std;
 using namespace ReyEngine;
@@ -9,21 +10,56 @@ int main(int argc, char** argv){
    Size<int> windowSize(800, 600);
    InitWindow(windowSize.x, windowSize.y, "basic drawsing test");
 
+   Camera2D camera = {};
+   camera.rotation = 0;
+   camera.offset.x = GetScreenWidth() / 2;
+   camera.offset.y = GetScreenHeight() / 2;
+   camera.target.x = camera.offset.x;
+   camera.target.y = camera.offset.y;
+   camera.zoom = 1.0f;
+   SetTargetFPS(60);
    while(!WindowShouldClose()){
+      int moveSpeed = 5;
+      Vec2<int> mvVec;
+      double rotation = 0;
+      if (InputManager::isKeyDown(InputInterface::KeyCodes::KEY_W)){
+         mvVec += {0, -1};
+      }
+      if (InputManager::isKeyDown(InputInterface::KeyCodes::KEY_A)) {
+         mvVec += {-1, 0};
+      }
+      if (InputManager::isKeyDown(InputInterface::KeyCodes::KEY_S)) {
+         mvVec += {0, 1};
+      }
+      if (InputManager::isKeyDown(InputInterface::KeyCodes::KEY_D)) {
+         mvVec += {1, 0};
+      }
+      if (InputManager::isKeyDown(InputInterface::KeyCodes::KEY_Q)) {
+         rotation += 1;
+      }
+      if (InputManager::isKeyDown(InputInterface::KeyCodes::KEY_E)) {
+         rotation -= 1;
+      }
+      if (mvVec) {
+         auto newVec = mvVec * moveSpeed;
+         camera.target.x += newVec.x;
+         camera.target.y += newVec.y;
+      }
+      if (rotation){
+         camera.rotation += rotation;
+      }
+
       BeginDrawing();
-      ClearBackground(GRAY);
-      BeginScissorMode(0,0,500,500);
-      DrawRectangle(0,0,9999,9999,RED);
-      DrawText("InScissor", 0,0, 20, BLACK);
-      BeginScissorMode(200,200,100,100);
-      DrawRectangle(0,0,9999,9999,BLUE);
-      BeginScissorMode(250,250,25,25);
-      DrawRectangle(0,0,9999,9999,GREEN);
-      EndScissorMode();
-      DrawText("OutScissor", 200,200,100,PURPLE);
-//      BeginScissorMode(0,0,0,0);
-//      DrawText("you shouldn't see this", 0,0,100,YELLOW);
-      EndScissorMode();
+      ClearBackground(WHITE);
+      DrawText("Foreground", 300, 300, 20, RED);
+      DrawRectangle(0,0,20,20,BLUE);
+      BeginMode2D(camera);
+      DrawText("Background", 100, 100, 20, BLACK);
+      DrawRectangle(100,200,20,20,GREEN);
+      EndMode2D();
+
+      DrawText("MoreForeground", 300, 400, 20, RED);
+
       EndDrawing();
 
    }
