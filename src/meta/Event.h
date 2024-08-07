c15561b0a77ff991f7bbc346f2749041c1419ff2
nick weishaar <n.weishaar@richmondelectronicdesign.com>
2024-08-06 22:17:01 -0400

    master - event ids are much more likely to be unique

--- src/meta/Event.h
+++ src/meta/Event.h
@@ -7,20 +7,51 @@
 #include <stdexcept>
 #include <iostream>
 #include "SharedFromThis.h"
+#include <iostream>
+template<size_t N, size_t I=0>
+struct hash_calc {
+    static constexpr size_t apply (const char (&s)[N]) {
+        return  (hash_calc<N, I+1>::apply(s) ^ s[I]) * 16777619u;
+    };
+};
 
-#define EVENT_GENERATE_UNIQUE_ID(CLASSNAME) static constexpr int CLASSNAME##_UNIQUE_ID = __COUNTER__; \
-static constexpr int getUniqueEventId(){return CLASSNAME##_UNIQUE_ID;}
-#define EVENT_GET_NAME(CLASSNAME) static std::string getEventName() {return #CLASSNAME + std::string("_") + std::to_string(CLASSNAME##_UNIQUE_ID);}
+template<size_t N>
+struct hash_calc<N,N> {
+    static constexpr size_t apply (const char (&s)[N]) {
+        return  2166136261u;
+    };
+};
+
+template<size_t N>
+constexpr size_t hash ( const char (&s)[N] ) {
+    return hash_calc<N>::apply(s);
+}
+
+#define STRINGIFY_DETAIL(x) #x
+#define STRINGIFY(x) STRINGIFY_DETAIL(x)
+
+#define CONCAT_DETAIL(x, y) x ## y
+#define CONCAT(x, y) CONCAT_DETAIL(x, y)
+
+#define CONCAT3_DETAIL(x, y, z) x ## y ## z
+#define CONCAT3(x, y, z) CONCAT3_DETAIL(x, y, z)
+
+#define EVENT_GENERATE_UNIQUE_ID(CLASSNAME) \
+    static constexpr long long unsigned int CLASSNAME##_UNIQUE_ID = hash(STRINGIFY(CONCAT3(CLASSNAME, PARENTCLASS, __COUNTER__))); \
+    static constexpr long long unsigned int getUniqueEventId() { return CLASSNAME##_UNIQUE_ID; }
+
+#define EVENT_GET_NAME(CLASSNAME) \
+    static std::string getEventName() { return #CLASSNAME + std::string("_") + std::to_string(CLASSNAME##_UNIQUE_ID); }
+
 #define EVENT_CTOR_SIMPLE(CLASSNAME, PARENTCLASS, ...) \
-EVENT_GENERATE_UNIQUE_ID(CLASSNAME)               \
-EVENT_GET_NAME(CLASSNAME)                                     \
-explicit CLASSNAME(const std::shared_ptr<ReyEngine::EventPublisher> publisher, ##__VA_ARGS__): PARENTCLASS(getUniqueEventId(), publisher)
+    EVENT_GENERATE_UNIQUE_ID(CLASSNAME) \
+    explicit CLASSNAME(const std::shared_ptr<ReyEngine::EventPublisher> publisher, ##__VA_ARGS__) : PARENTCLASS(CLASSNAME::getUniqueEventId(), publisher)
+
 //NOTE: event publisher is a pointer copy and not a reference because toEventPublisher expects an lvalue, which toEventPublisher is not;
 
 
 #define EVENT_CTOR_SIMPLE_OVERRIDABLE(CLASSNAME, PARENTCLASS) \
 EVENT_GENERATE_UNIQUE_ID(CLASSNAME)                           \
-EVENT_GET_NAME(CLASSNAME)                                     \
 explicit CLASSNAME(ReyEngine::EventId eventId, const std::shared_ptr<ReyEngine::EventPublisher>& publisher): PARENTCLASS(eventId, publisher)
 
 /////////////////////////////////////////////////////////////////////////////////////////
@@ -28,7 +59,7 @@ explicit CLASSNAME(ReyEngine::EventId eventId, const std::shared_ptr<ReyEngine::
 /////////////////////////////////////////////////////////////////////////////////////////
 namespace ReyEngine{
    class EventSubscriber;
-   using EventId = int;
+   using EventId = unsigned long long;
    class EventPublisher;
    class BaseEvent{
    public:
@@ -69,32 +100,35 @@ namespace ReyEngine{
    using EventCallbackMap = std::map<std::weak_ptr<EventSubscriber>,std::vector<EventHandler>, std::owner_less<>>;
    class EventPublisher : public inheritable_enable_shared_from_this<EventPublisher>{
    public:
-       void addSubscriber(std::weak_ptr<EventSubscriber> subscriber, std::string eventName, std::function<void(const BaseEvent&)> fx) {
+       template <typename T>
+       void addSubscriber(std::weak_ptr<EventSubscriber> subscriber, std::function<void(const BaseEvent&)> fx) {
            //assumed to be passing a valid subscriber pointer
            if (subscriber.expired()){
                throw std::runtime_error("Bad ptr!");
            }
-           auto _ev = _eventMap.find(eventName);
+           auto eventId = T::getUniqueEventId();
+           auto _ev = _eventMap.find(eventId);
            if (_ev == _eventMap.end()){
                //registered publisher doesn't have any events to publish
-               _eventMap[eventName] = EventCallbackMap();
+               _eventMap[eventId] = EventCallbackMap();
            }
 
            //get the vector of subscriber event maps
-           auto& subscribers = _eventMap[eventName]; //call again since iterator could be invalid
+           auto& subscribers = _eventMap[eventId]; //call again since iterator could be invalid
            auto found = subscribers.find(subscriber);
            if (found == subscribers.end()){
                //create new set
-               auto vec = _eventMap[eventName][subscriber];
+               _eventMap[eventId][subscriber];
            }
-           _eventMap[eventName][subscriber].push_back(fx);
+           _eventMap[eventId][subscriber].push_back(fx);
        }
        template <typename T>
        void publish(const T& event){
            static_assert(std::is_base_of_v<BaseEvent, T>); //compile time check
            auto publisher = downcasted_shared_from_this<EventPublisher>();
    //      std::cout << "Publishing eventType " << T::getUniqueEventId() << "(" << T::getEventName() << ")" << std::endl;
-           auto _ev = _eventMap.find(T::getEventName());
+           //publish by virtual eventId so we can publish from the base class
+           auto _ev = _eventMap.find(event.eventId);
            if (_ev == _eventMap.end()){
                //publisher doesn't have any events to publish
                return;
@@ -129,7 +163,7 @@ namespace ReyEngine{
    protected:
       std::shared_ptr<EventPublisher> toEventPublisher(){return downcasted_shared_from_this<EventPublisher>();}
    private:
-       std::map<std::string, EventCallbackMap> _eventMap;
+       std::map<int, EventCallbackMap> _eventMap;
    };
 
    /////////////////////////////////////////////////////////////////////////////////////////
@@ -146,7 +180,7 @@ namespace ReyEngine{
             auto s = (T&)baseEvent;
             typedEventHandler(s);
          };
-         publisher->addSubscriber(me, T::getEventName(), adapter);
+         publisher->addSubscriber<T>(me, adapter);
       };
       std::shared_ptr<EventSubscriber> toEventSubscriber(){return inheritable_enable_shared_from_this<EventSubscriber>::shared_from_this();}
    };
@@ -154,13 +188,4 @@ namespace ReyEngine{
    /////////////////////////////////////////////////////////////////////////////////////////
    /////////////////////////////////////////////////////////////////////////////////////////
    /////////////////////////////////////////////////////////////////////////////////////////
-
-
-   struct TestEvent1 : public Event<TestEvent1> {
-       EVENT_CTOR_SIMPLE(TestEvent1, Event<TestEvent1>){}
-   };
-
-   struct TestEvent2 : public Event<TestEvent2> {
-       EVENT_CTOR_SIMPLE(TestEvent2, Event<TestEvent2>){}
-   };
 }
