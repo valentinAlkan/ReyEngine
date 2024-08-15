f2badcdf7c5c8f1136fcba5d9410d084ffb0f17b
nick weishaar <nweishaar@harttech.com>
2024-08-15 09:52:32 -0400

    added some 3d stuff

--- src/3d/BaseBody.h
+++ src/3d/BaseBody.h
@@ -6,6 +6,7 @@
 
 namespace ReyEngine {
    class BaseBody;
+   class Viewport;
    namespace Internal {
       struct Transform3D{
 
@@ -20,19 +21,17 @@ namespace ReyEngine {
          Transform3D fromString(const std::string& str) override { return Transform3D();}
       };
 
-
-//      class BaseBody;
-
-
       //Something which renders 3D objects but is not a 3D body in itself
       class Renderable3D;
       class Renderer3D : public TypeContainer<Renderable3D>{
       public:
+         using TypeContainer<Renderable3D>::getChildren;
+         std::optional<std::shared_ptr<Viewport>> getViewport();
+      protected:
          Renderer3D(const std::string& name, const std::string& typeName)
          : TypeContainer<Renderable3D>(name, typeName)
          , NamedInstance(name, typeName)
          {}
-      protected:
          virtual void renderer3DBegin(){};
          virtual void renderer3DChain();
          virtual void renderer3DEnd(){};
@@ -40,28 +39,47 @@ namespace ReyEngine {
       };
 
       // Something which has volume is able to be rendered in 3D along with its children.
-      class Renderable3D : public Renderer3D {
+      class Renderable3D : public Internal::Component, public Renderer3D {
+      public:
+         using TypeContainer<Renderable3D>::getChildren;
+      protected:
          Renderable3D(const std::string& name, const std::string& typeName)
          : Renderer3D(name, typeName)
+         , Internal::Component(name, typeName)
          , NamedInstance(name, typeName)
          , _visible("visible")
          {}
-      protected:
          virtual void render3DBegin(){};
          virtual void render3D(){};
          virtual void render3DEnd(){};
          virtual void renderable3DChain();
          virtual void renderable3DEditorFeatures(){}
+         virtual void _init(){}
          BoolProperty _visible;
          Transform3D _transform;
          friend class Renderer3D;
+         friend class TypeContainer<Renderable3D>;
       };
    }
 
 
    //Combines the functionality above
-   class BaseBody : public Internal::Component {
-      REYENGINE_OBJECT_BUILD_ONLY(BaseBody, Internal::Component){}
+class BaseBody : public Internal::Renderable3D {
+public:
+   friend class ReyEngine::Internal::Component;
+   static constexpr char TYPE_NAME[] = "BaseBody";
+   std::string _get_static_constexpr_typename() override {return TYPE_NAME;}
+   using Internal::Renderable3D::getChildren;
+protected:
+   BaseBody(const std::string &name) : BaseBody(name, _get_static_constexpr_typename()) {}
+protected:
+   void _register_parent_properties()
+   override{
+      Internal::Component::_register_parent_properties();
+      Internal::Component::registerProperties();
+   }
+   BaseBody(const std::string &name, const std::string &typeName) : Internal::Renderable3D(name, typeName), NamedInstance(name, typeName)
+   {}
    protected:
       void _on_application_ready() override{}
       std::shared_ptr<BaseBody> toBaseBody();
--- src/3d/Camera3D.h
+++ src/3d/Camera3D.h
@@ -1,7 +1,48 @@
 #pragma once
+#include "BaseBody.h"
+#include "DrawInterface3D.h"
+#include "Viewport.h"
 
 namespace ReyEngine {
-   class Camera3D {
-
+   //Each canvas can have excatly 1 active camera at a time!!!!
+   class Camera3D : public BaseBody {
+      REYENGINE_OBJECT_BUILD_ONLY(Camera3D, BaseBody), PROPERTY_DECLARE(active, true){}
+      REYENGINE_DEFAULT_BUILD(ReyEngine::Camera3D)
+   public:
+      using BaseBody::getChildren;
+      void setRotation(double newRot);
+      double getRotation();
+      void setZoom(double newZoom);
+      double getZoom();
+      Pos3<double> nearToFar(const Pos3<double>&);
+      Pos3<double> farToNear(const Pos3<double>&);
+   protected:
+      void renderer3DBegin() override;
+      void renderer3DEnd() override;
+      void renderer3DChain() override;
+      void _on_enter_tree() override{
+         //look at 0,0,0
+         _camera.camera.position.x = 10;
+         _camera.camera.position.y = 10;
+         _camera.camera.position.z = 10;
+         _camera.camera.target.x = 0;
+         _camera.camera.target.y = 0;
+         _camera.camera.target.z = 0;
+         _camera.camera.up = {0,1,0};
+         auto optViewport = getViewport();
+         if (optViewport) optViewport.value()->setActiveCamera(_camera);
+      };
+      void _on_about_to_exit_tree() override{
+         ////center of screen
+         auto optViewport = getViewport();
+         if (!optViewport){
+            throw std::runtime_error("Camera " + getName() + " does not have a canvas to render to! That's not good! I'm curious how you managed to do that.");
+         }
+         auto& viewport = optViewport.value();
+         viewport->deleteActiveCamera();
+      }
+      BoolProperty active;
+   private:
+      CameraTransform3D _camera;
    };
 }
